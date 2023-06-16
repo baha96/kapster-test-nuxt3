e48c5bbcf35bd5fd4c1db3fc46821e5254bdfae2
baha96dev <baha.kaiyrbekov@musan.kz>
2023-06-16 18:25:36 +0600

    added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,24 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   // styles
   css: ["~/assets/styles/main.scss" ],
+
+  // options
+  modules: [
+    // ...
+    '@pinia/nuxt',
+  ],
+
+  // Pinia: options
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      'defineStore', // import { defineStore } from 'pinia'
+      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
 
   devtools: { enabled: true }
 })
--- plugins/useBootstrap.client.ts
+++ plugins/useBootstrap.client.ts
@@ -1,4 +1,4 @@
-import bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap'
 
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.provide('bootstrap', bootstrap)
